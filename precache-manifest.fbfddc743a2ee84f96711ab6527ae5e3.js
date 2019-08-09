48bf00bccaa64fed42b0dab089e00d1f9d6141c6
Ildar <53548114+ildar2b@users.noreply.github.com>
2019-08-09 18:52:53 +0300

    Add files via upload

---
+++ precache-manifest.fbfddc743a2ee84f96711ab6527ae5e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de02cb796f4a544691b",
+    "url": "/hello-nav-back-a/static/css/main.4f0654ec.chunk.css"
+  },
+  {
+    "revision": "7de02cb796f4a544691b",
+    "url": "/hello-nav-back-a/static/js/main.cecaf21f.chunk.js"
+  },
+  {
+    "revision": "49e009de662ff6081aad",
+    "url": "/hello-nav-back-a/static/js/runtime~main.13816d68.js"
+  },
+  {
+    "revision": "10875d29d3e88e23725d",
+    "url": "/hello-nav-back-a/static/css/2.4dbf8120.chunk.css"
+  },
+  {
+    "revision": "10875d29d3e88e23725d",
+    "url": "/hello-nav-back-a/static/js/2.18207673.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hello-nav-back-a/static/media/cat.4e1ec840.png"
+  },
+  {
+    "revision": "45931457f56a5395962e07d81f48a708",
+    "url": "/hello-nav-back-a/static/media/dog.45931457.jpg"
+  },
+  {
+    "revision": "e5699f7626d17fe76e2a2ff3781a9071",
+    "url": "/hello-nav-back-a/index.html"
+  }
+];
